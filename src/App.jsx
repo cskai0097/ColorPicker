42cc74b061e7092f93e2633992e874a728be5379
Saerione <chriss@Saeriones-MacBook-Air.local>
2024-01-29 14:37:04 -0800

    final_tested

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-// Write your Color component here
+import React,{ useState } from "react";
 
+// Write your Color component here
+const Color = ({color, setSelectedColor, isSelected}) => {
+  const className = isSelected ? `${color} selected` : color;
+  return (
+    <div 
+      className={className}
+      onClick={()=> setSelectedColor(color)}
+    ></div>
+  );
+};
+//picker component
+const Picker = ({selectedColor, setSelectedColor}) => {
+  return (
+  <div id="colors-list">
+    <Color color="red" isSelected={selectedColor === "red"} setSelectedColor={setSelectedColor} />
+    <Color color="blue" isSelected={selectedColor === "blue"} setSelectedColor={setSelectedColor}/>
+    <Color color="green" isSelected={selectedColor === "green"} setSelectedColor={setSelectedColor}/>
+  </div>
+  );
+};
+//app component
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
+
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor} style={{backgroundColor: selectedColor}}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <Picker selectedColor={selectedColor} setSelectedColor={setSelectedColor}/>
     </div>
   );
 };
